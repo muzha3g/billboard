c7b726a6bed6a026eeace689e3dc8af806ce0946
senji <146017825+muzha3g@users.noreply.github.com>
2024-02-25 20:27:54 +0800

    Update axios.js

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -16,26 +16,26 @@ instance.interceptors.request.use(function (config) {
   return config;
 });
 
-// instance.interceptors.response.use(
-//   function (response) {
-//     // Any status code that lie within the range of 2xx cause this function to trigger
-//     // Do something with response data
+instance.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
 
-//     return response;
-//   },
-//   function (error) {
-//     Swal.fire({
-//       icon: "error",
-//       title: "Oops...",
-//       text: "Something went wrong!",
-//       footer: `<a href="#">${error.message}</a>`,
-//     });
-//     console.log(error);
-//     sessionStorage.clear();
-//     // Any status codes that falls outside the range of 2xx cause this function to trigger
-//     // Do something with response error
-//     return Promise.reject(error);
-//   }
-// );
+    return response;
+  },
+  function (error) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Something went wrong!",
+      footer: `<a href="#">${error.message}</a>`,
+    });
+    console.log(error);
+    sessionStorage.clear();
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
